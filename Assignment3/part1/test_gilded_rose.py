e2d0b7e628f5c8355abce805d59afaa3295826ed
zhiyizhang <zhang.zhiyi3@northeastern.edu>
2023-10-05 19:25:36 -0700

    assignment3

--- Assignment3/part1/test_gilded_rose.py
+++ Assignment3/part1/test_gilded_rose.py
@@ -5,11 +5,46 @@ from gilded_rose import Item, GildedRose
 
 
 class GildedRoseTest(unittest.TestCase):
-    def test_foo(self):
-        items = [Item("foo", 0, 0)]
+    def test_normal(self):
+        items = [Item("apple", 10, 20)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
-        self.assertEquals("fixme", items[0].name)
+        self.assertEquals("apple", items[0].name)
+        self.assertEquals(9, items[0].sell_in)
+        self.assertEquals(19, items[0].quality)
+
+    def test_aged_brie(self):
+        items = [Item("Aged Brie", 2, 0)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual("Aged Brie", items[0].name)
+        self.assertEqual(1, items[0].sell_in)
+        self.assertEqual(1, items[0].quality)
+
+    def test_sulfuras(self):
+        items = [Item("Sulfuras, Hand of Ragnaros", 0, 80)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual("Sulfuras, Hand of Ragnaros", items[0].name)
+        self.assertEqual(0, items[0].sell_in)
+        self.assertEqual(80, items[0].quality)
+
+
+    def test_backstage_passes(self):
+        items = [Item("Backstage passes to a TAFKAL80ETC concert", 15, 20)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual("Backstage passes to a TAFKAL80ETC concert", items[0].name)
+        self.assertEqual(14, items[0].sell_in)
+        self.assertEqual(21, items[0].quality)
+
+    def test_conjured_item(self):
+        items = [Item("Conjured", 5, 10)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual("Conjured", items[0].name)
+        self.assertEqual(4, items[0].sell_in)
+        self.assertEqual(8, items[0].quality)
 
 
 if __name__ == '__main__':
